f1f56cf468a51f558e5edec9b2d719787618e74b
ALEXSAFTERSHOCK\alexs <alexsmits247@gmail.com>
2025-05-13 23:11:20 +1000

    Redid how camera behaves in the level select, it now starts on the correct screen when leaving a level.

---
+++ Dragon's Hoard/Assets/Scripts/LevelSelectCamera.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+
+public class LevelSelectCamera : MonoBehaviour
+{
+    public ProgressSaver playerProgressScriptableObject;
+    public GameObject cameraObject;
+    public GameObject waterFall;
+    public float activeScreen = 1;
+    public float moveSpeed = 1;
+
+    [Header("Screen Transforms")]
+    public Vector3 screen1transform;
+    public Vector3 screen2transform;
+    public Vector3 screen3transform;
+    
+    [Header("UI Arrows")]
+    public GameObject screen1Arrows;
+    public GameObject screen2Arrows;
+    public GameObject screen3Arrows;
+
+    void Start()
+    {
+        if (playerProgressScriptableObject.cameraPosition == 1)
+        {
+            activeScreen = 1;
+            cameraObject.transform.position = screen1transform;
+        }    
+        else if (playerProgressScriptableObject.cameraPosition == 2)
+        {
+            activeScreen = 2;
+            cameraObject.transform.position = screen2transform;
+        }
+        else
+        {
+            activeScreen = 3;
+            cameraObject.transform.position = screen3transform;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (activeScreen == 1)
+        {
+            cameraObject.transform.position = Vector3.Lerp(cameraObject.transform.position, screen1transform, moveSpeed * Time.deltaTime);
+            screen1Arrows.SetActive(true);
+        }
+        else
+        {
+            screen1Arrows.SetActive(false);
+        }
+        
+        if (activeScreen == 2)
+        {
+            cameraObject.transform.position = Vector3.Lerp(cameraObject.transform.position, screen2transform, moveSpeed * Time.deltaTime);
+            waterFall.SetActive(true);
+            screen2Arrows.SetActive(true);
+        }
+        else
+        {
+            waterFall.SetActive(false);
+            screen2Arrows.SetActive(false);
+        }
+        
+        if (activeScreen == 3)
+        {
+            cameraObject.transform.position = Vector3.Lerp(cameraObject.transform.position, screen3transform, moveSpeed * Time.deltaTime);
+            screen3Arrows.SetActive(true);
+        }
+        else
+        {
+            screen3Arrows.SetActive(false);
+        }
+
+    }
+
+    public void Screen1()
+    {
+        activeScreen = 1;
+        playerProgressScriptableObject.cameraPosition = 1;
+    }
+
+    public void Screen2()
+    {
+        activeScreen = 2;
+        playerProgressScriptableObject.cameraPosition = 2;
+    }
+
+    public void Screen3()
+    {
+        activeScreen = 3;
+        playerProgressScriptableObject.cameraPosition = 3;
+    }
+
+}
--- Dragon's Hoard/Assets/Scripts/PhysicalButton.cs
+++ Dragon's Hoard/Assets/Scripts/PhysicalButton.cs
@@ -5,11 +5,19 @@ public class PhysicalButton : MonoBehaviour
     public GameObject button;
     public GameObject lightIndicator;
 
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter2D(Collider2D _colliderEnter)
     {
         if (_colliderEnter.gameObject == button)
         {
             lightIndicator.SetActive(true);
+            _audioSource.Play();
         }
     }
 
@@ -18,6 +26,7 @@ public class PhysicalButton : MonoBehaviour
         if (_colliderExit.gameObject == button)
         {
             lightIndicator.SetActive(false);
+            _audioSource.Play();
         }
     }
 }
